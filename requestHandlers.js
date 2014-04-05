7af90a111a363ab2af5882b00930ba52d6d59d19
Bondifrench <d_dumaine@yahoo.com>
2014-04-05 16:39:15 +1100

    Implemented last lines of code but upload doesn't work on windows

--- requestHandlers.js
+++ requestHandlers.js
@@ -1,31 +1,43 @@
 #!/usr/bin/env node
 "use strict";
-var querystring = require("querystring"),
-	fs = require("fs"),
-	filename = "./index.html";
+var fs = require("fs"),
+	formidable = require("formidable");
+var filename = "./index.html";
 
-function start (resp, postData) {
+function start (resp) {
 	console.log("Request Handler 'Start' was called");
-
-	resp.writeHead(200, {"Content-Type":"text/html"});
 	fs.readFile(filename, function (err, data) {
 		if (err) throw err;
+		resp.writeHead(200, {"Content-Type":"text/html"});
 		resp.write(data);
 		resp.end();
 		});
 	}
 
-function upload (resp, postData) {
+function upload (resp, req) {
 	console.log("Request Handler 'Upload' was called");
-
-	resp.writeHead(200, {"Content-Type":"text/plain"});
-	resp.write("You've sent the text:\n\n " + querystring.parse(postData).text );
-	resp.end();
+	var form = new formidable.IncomingForm();
+	console.log("about to parse");
+	form.parse(req, function (error, fields, files) {
+		console.log("parsing done");
+		/* Possible error on Windows systems: tried to rename to an already existing file */
+		/* for the moment this solution doesn't work on Windows */
+		fs.rename(files.upload.path, "/tmp/test.png", function (error) {
+			if (error) {
+				fs.unlink("/tmp/test.png");
+				fs.rename(files.upload.path, "/tmp/test.png");
+			}
+		});
+		resp.writeHead(200, {"Content-Type":"text/html"});
+		resp.write("Received image:<br/>");
+		resp.write("<img src=/show />");
+		resp.end();
+		});
 }
 
-function show (resp, postData) {
+function show (resp) {
 	console.log("Request handler 'show' was called");
-	fs.readFile("/tmp/test.png", "binary", function (error, file) {
+	fs.readFile("./tmp/test.png", "binary", function (error, file) {
 		if (error) {
 			resp.writeHead(500, {"Content-type":"text/plain"});
 			resp.write(error + "\n");
@@ -37,6 +49,7 @@ function show (resp, postData) {
 		}
 	});
 }
+
 exports.start=start;
 exports.upload=upload;
 exports.show = show;
